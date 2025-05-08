eabfc76692138795fc7fc3f658eb2f724b2b041c
Shobhit Kumar Sharma <sharmashobhit1000@gmail.com>
2025-05-08 18:23:56 +0530

    Ready for Deployment

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,19 +1,49 @@
-import { useUser } from "@clerk/clerk-react";
-import { Navigate, useLocation } from "react-router-dom";
+// import { useUser } from "@clerk/clerk-react";
+// import { Navigate, useLocation } from "react-router-dom";
+
+// const ProtectedRoute = ({ children }) => {
+//   const { isSignedIn, user, isLoaded } = useUser();
+//   const { pathname } = useLocation();
+
+//   console.log("isLoaded", isLoaded, "isSignedIn", isSignedIn, "user", user, "pathname", pathname);
+//   if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+//     return <Navigate to="/?sign-in=true" />;
+//   }
+
+//   // Check onboarding status
+//   if (
+//     user !== undefined 
+//     &&
+//     !user?.unsafeMetadata?.role &&
+//     pathname !== "/onboarding"
+//   ) {
+//     return <Navigate to="/onboarding" />;
+//   }
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
+
+
+
+
+
 
 const ProtectedRoute = ({ children }) => {
   const { isSignedIn, user, isLoaded } = useUser();
   const { pathname } = useLocation();
 
   console.log("isLoaded", isLoaded, "isSignedIn", isSignedIn, "user", user, "pathname", pathname);
-  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
+
+  if (isLoaded && !isSignedIn) {
     return <Navigate to="/?sign-in=true" />;
   }
 
-  // Check onboarding status
   if (
-    user !== undefined 
-    &&
+    isSignedIn &&
+    user &&
     !user?.unsafeMetadata?.role &&
     pathname !== "/onboarding"
   ) {
@@ -22,5 +52,3 @@ const ProtectedRoute = ({ children }) => {
 
   return children;
 };
-
-export default ProtectedRoute;
